b354df2cc9caa09de6a18451e120779e2e587e86
José Ramos <jrcmramos@gmail.com>
2020-05-06 11:38:05 +0200

    Add README description

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.5")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -18,7 +18,7 @@ let package = Package(
         .target(
             name: "ColorMatcher",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
         ]),
         .testTarget(
             name: "ColorMatcherTests",
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # ColorMatcher
 
-A description of this package.
+
+## Description
+
+This command line tool gives you the most approximate color from an input against a color palette.
+The main purpose of this project was to provide an easy tool to follow or readjust colors with project theme guidelines.
+
+## Usage 
+
+In order to use this tool, you need to provide a `spec.json` and either an hex value (`--hex`) or a input source  (`--input-colors-path`) with the same format as the spec.
+  
+  
+`swift run color-matcher distance ./Examples/spec.json --hex 0x111000 --input-colors-path Examples/input.json --results-folder ./Results`
